aa84639d87b7e2127917505297f01418ca8fce25
lizardkingLK <chansanfdo@gmail.com>
2022-10-22 20:46:48 +0530

    added parenthesis functionality. changed keyboard layout. added Delete key (backspace)

--- src/App.js
+++ src/App.js
@@ -1,73 +1,57 @@
-import {useState} from 'react';
+/* eslint-disable no-eval */
+import { useState } from 'react';
 
 function App() {
 
   //calculator function
-  const[calc, setCalc] = useState("");
-  const[result, setResult] = useState("");
-
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
   const options = ["/", "*", "+", "-"];
 
-  const updateCalculation = value =>{
-    if(options.includes(value) && calc === "" || options.includes(value) && options.includes(calc.slice(-1)))
-    {
-      return ;
+  const updateCalculation = (value) => {
+    if (options.includes(value) && (!calc || options.includes(calc.slice(-1)))) {
+      return;
     }
-    setCalc(calc + value);
-
-    if(!options.includes(value)){
-      setResult(eval(calc + value).toString());
+    const result = calc + value;
+    switch (value) {
+      case "=":
+        setResult(`${eval(result)}`);
+        break;
+      case "DEL":
+        const extracted = calc.substring(0, calc.length - 1);
+        setCalc(extracted);
+        setResult(`${eval(extracted)}`);
+        break;
+      case "CLR":
+        setCalc("");
+        setResult("");
+        break;
+      default:
+        setCalc(result);
+        setResult(`${eval(result)}`);
+        break;
     }
   }
 
-//button adding
-  const addDigits = () => {
-    const digits = [];
+  //render buttons
+  const getButtons = () => {
+    const buttons = ['(', ')', 'DEL', '/']
+      .concat(Array(3).fill().map((_, i) => `${i + 7}`)).concat('*')
+      .concat(Array(3).fill().map((_, i) => `${i + 4}`)).concat('+')
+      .concat(Array(3).fill().map((_, i) => `${i + 1}`)).concat('-')
+      .concat('CLR', '0', '.', '=');
 
-    for(let j=1 ;j < 10;j++){
-      digits.push(
-        <button onClick={() => updateCalculation(j.toString())} key={ j }> { j }</button>
-      )
-    }
-    return digits;
+    return buttons.map((label, index) => (
+      <button key={index} onClick={() => updateCalculation(label)}>{label}</button>)
+    );
   }
-
-  const equation = () => {
-    setCalc(eval(calc).toString());
-  }
-
-  const deleteLast = () => {
-    if(calc == ""){
-      return ;
-    }
-    const value = calc.slice(0, -1);
-    setCalc(value);
-  }
-
-
 
   return (
     <div className="App">
       <div className="calculator">
-        <div className="display">
-         { result ? <span>({result})</span> : ""} &nbsp; {calc || "0"}
-        </div>
-        <div className="operators">
-          <button onClick={() => updateCalculation('/')}>/</button>
-          <button onClick={() => updateCalculation('*')}>*</button>
-          <button onClick={() => updateCalculation('+')}>+</button>
-          <button onClick={() => updateCalculation('-')}>-</button>
-
-          <button onClick={deleteLast}>DEL</button>
-        </div>
-        <div className="digits">
-          
-          { addDigits() }
-
-          <button onClick={() => updateCalculation('0')}>0</button>
-          <button onClick={() => updateCalculation('.')}>.</button>
-          <button onClick={equation}>=</button>
-        </div>
+        <div className="solution">{result ? <span>({result})</span> : ""}</div>
+        <div className="display">{calc || "0"}</div>
+        <div className="keyboard">{getButtons()}</div>
       </div>
     </div>
   );
